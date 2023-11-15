d08573faea5902edc61ad17c0aa6f5b43af21992
Vadim Yarosh <vadzim_yarosh@senla.eu>
2023-11-15 11:30:18 +0300

    Added to do file. Added some dmls for the product microservice. Added security. Added debug profile to disable security during debug.

--- product/run/src/main/java/com/shop/product/run/RunProductService.java
+++ product/run/src/main/java/com/shop/product/run/RunProductService.java
@@ -1,15 +1,22 @@
 package com.shop.product.run;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
-        "com.shop.product"
+        "com.shop.product",
+        "com.shop.common.utils.all.config.security",
+        "com.shop.common.utils.filter"
 })
+@EnableFeignClients(basePackages = {"com.shop.product.client", "com.shop.authorization.client"})
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = {"com.shop.product.dao"})
 @EntityScan(basePackages = {"com.shop.product.model"})
 public class RunProductService {
