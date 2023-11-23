123940805fde0fefe0b0087d8e455053881be21f
Vadim Yarosh <vadzim_yarosh@senla.eu>
2023-11-23 17:36:52 +0300

    Bug fixes.

--- shop/dao/src/main/java/com/shop/shop/dao/ShopProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shop.shop.dao;
-
-import com.shop.shop.model.ShopProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopProductRepository extends JpaRepository<ShopProduct, Long> {
-}
